347dbab115b9a2d391676eeeb0b70cffe0d1c74d
lightbullet001 <lightbullet001@gmail.com>
2022-01-12 08:15:50 +0700

    lan 2

--- src/oo/Employee.java
+++ src/oo/Employee.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class Employee{
 //data
 String name = Khoa;
-
+int salary = 4000000;
 //funtion
 
 void show(){
     System.out.println("Tên Nhân Viên: " +name);
+    System.out.println("Lương Tháng: " +salary);
 }
 }
